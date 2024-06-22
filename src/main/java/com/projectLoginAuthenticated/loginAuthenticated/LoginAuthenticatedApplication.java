ec892942aafad571f73844c4f26496f5c255c262
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2024-06-22 01:33:59 -0300

    Project spring security authentication using User and password with Spring MVC, Thymeleaf

---
+++ src/main/java/com/projectLoginAuthenticated/loginAuthenticated/LoginAuthenticatedApplication.java
@@ -0,0 +1,13 @@
+package com.projectLoginAuthenticated.loginAuthenticated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthenticatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthenticatedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectLoginAuthenticated/loginAuthenticated/LoginAuthenticatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectLoginAuthenticated.loginAuthenticated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthenticatedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
